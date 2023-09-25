99488c63522facd85b7ca3611c21caefc43d70ad
Vinay Shah <shahvinay1211@gmail.com>
2023-09-25 10:56:48 -0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,425 @@
+$(document).ready(function () {
+
+    $('#menu').click(function () {
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load', function () {
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if (window.scrollY > 60) {
+            document.querySelector('#scroll-top').classList.add('active');
+        } else {
+            document.querySelector('#scroll-top').classList.remove('active');
+        }
+
+        // scroll spy
+        $('section').each(function () {
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let top = $(window).scrollTop();
+            let id = $(this).attr('id');
+
+            if (top > offset && top < offset + height) {
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+        });
+    });
+
+    // smooth scrolling
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top,
+        }, 500, 'linear')
+    });
+
+
+    
+    // <!-- emailjs to mail contact form data -->
+    $("#contact-form").submit(function (event) {
+        emailjs.init("y3uVObpo0LxLWpA2u");
+
+        // Capture form data before sending the form
+        var userName = this.user_name.value;
+        var userEmail = this.user_email.value;
+        var userContact = this.contact_number.value;
+        var userMessage = this.message.value;
+
+        emailjs.send('service_z6ovmnu', 'template_9otfujy', {
+            from_name: userName,
+            email_id: userEmail,
+            contact : userContact,
+            message: userMessage
+        })
+            .then(function (response) {
+                console.log('SUCCESS!', response.status, response.text);
+                document.getElementById("contact-form").reset();
+                alert("Form Submitted Successfully");
+            }, function (error) {
+                console.log('FAILED...', error);
+                alert("Form Submission Failed! Try Again");
+            });
+        event.preventDefault();
+    });
+    // <!-- emailjs to mail contact form data -->
+
+});
+
+document.addEventListener('visibilitychange',
+    function () {
+        if (document.visibilityState === "visible") {
+            document.title = "Portfolio | Vinay Shah";
+            $("#favicon").attr("href", "assets/images/favicon2.png");
+        }
+        else {
+            document.title = "Come Back To Portfolio";
+            $("#favicon").attr("href", "assets/images/favicon.png");
+        }
+    });
+
+
+// <!-- typed js effect starts -->
+var typed = new Typed(".typing-text", {
+    strings: ["Software Engineer", "Full Stack Engineer", "Data Analyst","Data Engineer","Front-End Engineer", "Back-End Engineer"],
+    loop: true,
+    typeSpeed: 30,
+    backSpeed: 25,
+    backDelay: 500,
+});
+// <!-- typed js effect ends -->
+
+async function fetchData(type = "skills") {
+    let response
+    type === "skills" ?
+        response = [
+            {
+                "name": "Python",
+                "icon": "https://img.icons8.com/color/48/000000/python--v1.png"
+            },{
+                "name" : "C",
+                "icon" : "https://img.icons8.com/color/48/c-programming.png"
+            },{
+                "name" : "JavaScript",
+                "icon" : "https://img.icons8.com/color/48/javascript--v1.png"
+            },{
+                "name" : "Java",
+                "icon" : "https://img.icons8.com/color/48/java-coffee-cup-logo--v1.png"
+            },
+            {
+                "name": "MySQL",
+                "icon": "https://img.icons8.com/color/48/000000/mysql-logo.png"
+            },{
+                "name" : "Spring",
+                "icon" : "https://img.icons8.com/color/48/spring-logo.png"
+            },{
+                "name" : "jQuery",
+                "icon" : "https://img.icons8.com/external-tal-revivo-filled-tal-revivo/24/external-jquery-is-a-javascript-library-designed-to-simplify-html-logo-filled-tal-revivo.png"
+            },{
+                "name" : "Pandas",
+                "icon" : "https://img.icons8.com/color/48/pandas.png"
+            },{
+                "name" : "HTML",
+                "icon" : "https://img.icons8.com/color/48/html-5--v1.png"
+            },{
+                "name" : "CSS",
+                "icon" : "https://img.icons8.com/color/48/css.png"
+            },{
+                "name" : "React",
+                "icon" : "https://img.icons8.com/color/48/react-native.png"
+            },{
+                "name" : "Node",
+                "icon" : "https://img.icons8.com/color/48/nodejs.png"
+            },{
+                "name" : "Flask",
+                "icon" : "https://img.icons8.com/color/48/flask.png"
+            },{
+                "name" : "REST",
+                "icon" : "https://img.icons8.com/color/48/api-settings.png"
+            },{
+                "name" : "AWS",
+                "icon" : "https://img.icons8.com/color/48/amazon-web-services.png"
+            },{
+                "name" : "MongoDB",
+                "icon" : "https://img.icons8.com/color/48/mongodb.png"
+            },{
+                "name" : "Firebase",
+                "icon" : "https://img.icons8.com/color/48/google-firebase-console.png"
+            },{
+                "name" : "Git",
+                "icon" : "https://img.icons8.com/color/48/git.png"
+            },{
+                "name" : "Postman",
+                "icon" : "https://img.icons8.com/pulsar-color/48/postman-api.png"
+            },{
+                "name" : "Apache Kafka",
+                "icon" : "https://img.icons8.com/external-tal-revivo-color-tal-revivo/24/external-apache-a-free-and-open-source-cross-platform-web-server-software-logo-color-tal-revivo.png"
+            },{
+                "name" : "NumPy",
+                "icon" : "https://img.icons8.com/color/48/numpy.png"
+            },
+            {
+                "name": "Tableau",
+                "icon": "https://img.icons8.com/color/48/tableau-software.png"
+            }, 
+            {
+                "name": "PowerBI",
+                "icon": "https://img.icons8.com/color/48/power-bi.png"
+            },
+            {
+                "name": "Visio",
+                "icon": "https://img.icons8.com/color/48/microsoft-visio.png"
+            },
+            {
+                "name": "VS Code",
+                "icon": " https://img.icons8.com/fluency/48/visual-studio.png"
+            },
+            {
+                "name": "WordPress",
+                "icon": "https://img.icons8.com/color/48/000000/wordpress.png"
+            },  
+            {
+                "name": "Salesforce",
+                "icon": "https://img.icons8.com/color/48/salesforce.png"
+            },
+            {
+                "name": "Leadership",
+                "icon": "https://img.icons8.com/color/48/meeting-room.png" 
+            }
+        ]
+        :
+        response =[
+            {
+              "name": "Diabetic Retinopathy Detection",
+              "desc": "Covered diseases caused by diabetis through retinal Images",
+              "image": "Diabetic",
+              "category": "visual",
+              "links": {
+                "view": "https://github.com/Vinay0909/DiabeticRetinopathyDetection",
+                "code": "https://github.com/Vinay0909/DiabeticRetinopathyDetection"
+              }
+            },{
+                "name": "Worker Finder",
+                "desc": "Application that could find plumber,electrician and other within area",
+                "image": "worker",
+                "category": "visual",
+                "links": {
+                  "view": "https://github.com/Vinay0909/Worker-Job",
+                  "code": "https://github.com/Vinay0909/Worker-Job"
+                }
+              },{
+                "name": "Ecommerce Data Analysis",
+                "desc": "Referes to strategy designed to analyze large amounts of data to produce meaningful insights and measure behaviour,performance,trends and ROI",
+                "image": "Ecommerce",
+                "category": "visual",
+                "links": {
+                  "view": "https://github.com/Vinay0909/Ecommerce-Analysis",
+                  "code": "https://github.com/Vinay0909/Ecommerce-Analysis"
+                },
+              },{
+                "name": "Advice Web Application",
+                "desc": "This project focuses on solving the queries of the people by answering it from the Api when fetched",
+                "image": "API",
+                "category": "visual",
+                "links": {
+                  "view": "https://github.com/Vinay0909/advice_app-react",
+                  "code": "https://github.com/Vinay0909/advice_app-react"
+                },
+              },{
+                "name": "Wordle",
+                "desc": " A real time movies information portal containg all the flims from start to recent ongoin ones with detailed information",
+                "image": "Movie",
+                "category": "visual",
+                "links": {
+                  "view": "https://github.com/Vinay0909/MoviesInfo",
+                  "code": "https://github.com/Vinay0909/MoviesInfo"
+                },
+              },{
+                "name": "The Friendly Couch",
+                "desc": "A support App for users to seek assistance from their network or professionals, promoting help-seeking and a safe environment.",
+                "image": "friendly-couch",
+                "category": "visual",
+                "links": {
+                  "view": "https://github.com/Ayush-corp/the_friendly_couch",
+                  "code": "https://github.com/Ayush-corp/the_friendly_couch"
+                },
+              }]
+    const data = await response;
+    return data;
+}
+
+function showSkills(skills) {
+    let skillsContainer = document.getElementById("skillsContainer");
+    let skillHTML = "";
+    skills.forEach(skill => {
+        skillHTML += `
+        <div class="bar">
+              <div class="info">
+                <img src=${skill.icon} alt="skill" />
+                <span>${skill.name}</span>
+              </div>
+            </div>`
+    });
+    skillsContainer.innerHTML = skillHTML;
+}
+
+// ../images/projects/${project.image}.png
+
+function showProjects(projects) {
+    let projectsContainer = document.querySelector("#work .box-container");
+    let projectHTML = "";
+    projects.slice(0, 10).filter(project => project.category != "android").forEach(project => {
+        projectHTML += `
+        <div class="box tilt">
+      <img draggable="false" src="./assets/images/projects/${project.image}.png" alt="projecthello" />
+      
+      <div class="content">
+        <div class="tag">
+        <h3>${project.name}</h3>
+        </div>
+        <div class="desc">
+          <p>${project.desc}</p>
+          <div class="btns">
+            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View/Code</a>
+          </div>
+        </div>
+      </div>
+    </div>`
+    });
+    projectsContainer.innerHTML = projectHTML;
+
+    // <!-- tilt js effect starts -->
+    VanillaTilt.init(document.querySelectorAll(".tilt"), {
+        max: 15,
+    });
+    // <!-- tilt js effect ends -->
+
+    /* ===== SCROLL REVEAL ANIMATION ===== */
+    const srtop = ScrollReveal({
+        origin: 'top',
+        distance: '80px',
+        duration: 1000,
+        reset: true
+    });
+
+    /* SCROLL PROJECTS */
+    srtop.reveal('.work .box', { interval: 200 });
+
+}
+
+fetchData().then(data => {
+    showSkills(data);
+});
+
+fetchData("projects").then(data => {
+    showProjects(data);
+});
+
+// <!-- tilt js effect starts -->
+VanillaTilt.init(document.querySelectorAll(".tilt"), {
+    max: 15,
+});
+// <!-- tilt js effect ends -->
+
+
+// pre loader start
+// function loader() {
+//     document.querySelector('.loader-container').classList.add('fade-out');
+// }
+// function fadeOut() {
+//     setInterval(loader, 500);
+// }
+// window.onload = fadeOut;
+// pre loader end
+
+// disable developer mode
+document.onkeydown = function (e) {
+    if (e.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
+
+
+
+
+/* ===== SCROLL REVEAL ANIMATION ===== */
+const srtop = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 1000,
+    reset: true
+});
+
+/* SCROLL HOME */
+srtop.reveal('.home .content h3', { delay: 200 });
+srtop.reveal('.home .content p', { delay: 200 });
+srtop.reveal('.home .content .btn', { delay: 200 });
+
+srtop.reveal('.home .image', { delay: 400 });
+srtop.reveal('.home .linkedin', { interval: 600 });
+srtop.reveal('.home .github', { interval: 700 });
+srtop.reveal('.home .envelope', { interval: 800 });
+srtop.reveal('.home .twitter', { interval: 1000 });
+srtop.reveal('.home .telegram', { interval: 600 });
+srtop.reveal('.home .instagram', { interval: 600 });
+srtop.reveal('.home .dev', { interval: 600 });
+
+/* SCROLL ABOUT */
+srtop.reveal('.about .content h3', { delay: 200 });
+srtop.reveal('.about .content .tag', { delay: 200 });
+srtop.reveal('.about .content p', { delay: 200 });
+srtop.reveal('.about .content .box-container', { delay: 200 });
+srtop.reveal('.about .content .resumebtn', { delay: 200 });
+
+
+/* SCROLL SKILLS */
+srtop.reveal('.skills .container', { interval: 200 });
+srtop.reveal('.skills .container .bar', { delay: 400 });
+
+/* SCROLL EDUCATION */
+srtop.reveal('.education .box', { interval: 200 });
+
+/* SCROLL PROJECTS */
+srtop.reveal('.work .box', { interval: 200 });
+
+/* SCROLL EXPERIENCE */
+srtop.reveal('.experience .timeline', { delay: 400 });
+srtop.reveal('.experience .timeline .container', { interval: 400 });
+
+/* SCROLL CONTACT */
+srtop.reveal('.contact .container', { delay: 400 });
+srtop.reveal('.contact .container .form-group', { delay: 400 });
+
+const mediaQuery = window.matchMedia('(min-width: 300px)')
+// Check if the media query is true
+if (mediaQuery.matches) {
+  // Then trigger an alert
+  
+    srtop.reveal('.education .box', { interval: 100 });
+
+    /* SCROLL PROJECTS */
+    srtop.reveal('.work .box', { interval: 100 });
+
+    /* SCROLL EXPERIENCE */
+    srtop.reveal('.experience .timeline', { delay: 200 });
+    srtop.reveal('.experience .timeline .container', { interval: 200 });
+
+    /* SCROLL CONTACT */
+    srtop.reveal('.contact .container', { delay: 200 });
+    srtop.reveal('.contact .container .form-group', { delay: 200 });
+}
